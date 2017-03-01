d7b88b55d193c584c5844afe56e7010a85d2f95d
leondutoit <dutoit.leon@gmail.com>
2017-03-01 10:39:52 +0100

    tornado example now support all upload methods

--- tornado-example.py
+++ tornado-example.py
@@ -1,5 +1,6 @@
 # https://gist.github.com/bdarnell/5bb1bd04a443c4e06ccd
 
+import os
 import logging
 import tornado.queues
 
@@ -15,33 +16,60 @@ define('port', default=8888)
 define('debug', default=True)
 define('server_delay', default=0)
 define('num_chunks', default=50)
-define('max_body_size', 1024*1024*1024)
+define('max_body_size', 1024*1024*1024*5)
+
+UPLOADS_FOLDER = '/Users/leondutoit/uploaded-files'
+
+class FormDataHandler(RequestHandler):
+
+    def prepare(self):
+        # called before verb on each request
+        self.filename = ''
+
+    def post(self):
+        if len(self.request.files['file']) > 1:
+            # only allow one file per request for now
+            self.send_error()
+        # TODO: check filename
+        filename = self.request.files['file'][0]['filename']
+        target = os.path.normpath(UPLOADS_FOLDER + '/' + filename)
+        filebody = self.request.files['file'][0]['body']
+        with open(target, 'ab+') as f:
+            f.write(filebody)
+        self.write({'message': 'file uploaded'})
+
+    def on_finish(self):
+        # called after response is returned to client
+        # could notify an external worker here
+        pass
 
 @stream_request_body
 class UploadHandler(RequestHandler):
+
     def prepare(self):
         logging.info('UploadHandler.prepare')
 
     @gen.coroutine
     def data_received(self, chunk):
-        logging.info('UploadHandler.data_received(%d bytes: %r)',
-                     len(chunk), chunk[:9])
+        #logging.info('UploadHandler.data_received(%d bytes: %r)', len(chunk), chunk[:9])
         with open('out', 'ab+') as f:
             f.write(chunk)
-        yield gen.Task(IOLoop.current().call_later, options.server_delay)
+        # could use this to slow the client down if needed
+        # yield gen.Task(IOLoop.current().call_later, options.server_delay)
 
-    def put(self):
-        logging.info('UploadHandler.put')
+    def post(self):
+        logging.info('UploadHandler.post')
         self.write('ok')
 
 @stream_request_body
 class ProxyHandler(RequestHandler):
+
     def prepare(self):
         logging.info('ProxyHandler.prepare')
         self.chunks = tornado.queues.Queue(1)
         self.fetch_future = AsyncHTTPClient().fetch(
-            'http://localhost:%d/upload' % options.port,
-            method='PUT',
+            'http://localhost:%d/upload_stream' % options.port,
+            method='POST',
             body_producer=self.body_producer,
             request_timeout=12000.0)
 
@@ -55,13 +83,12 @@ class ProxyHandler(RequestHandler):
 
     @gen.coroutine
     def data_received(self, chunk):
-        logging.info('ProxyHandler.data_received(%d bytes: %r)',
-                     len(chunk), chunk[:9])
+        #logging.info('ProxyHandler.data_received(%d bytes: %r)', len(chunk), chunk[:9])
         yield self.chunks.put(chunk)
 
     @gen.coroutine
-    def put(self):
-        logging.info('ProxyHandler.put')
+    def post(self):
+        logging.info('ProxyHandler.post')
         # Write None to the chunk queue to signal body_producer to exit,
         # then wait for the request to finish.
         yield self.chunks.put(None)
@@ -69,20 +96,12 @@ class ProxyHandler(RequestHandler):
         self.set_status(response.code)
         self.write(response.body)
 
-@stream_request_body
-class Mine(tornado.web.RequestHandler):
-    def data_received(self, chunk):
-        with open('boom', 'wb+') as f:
-            f.write(chunk)
-    def post(self):
-        self.write({'m': 'yep'})
-
 def main():
     parse_command_line()
     app = Application([
-        ('/upload', UploadHandler),
-        ('/proxy', ProxyHandler),
-        ('/proxy', Mine),
+        ('/upload_stream', UploadHandler),
+        ('/stream', ProxyHandler),
+        ('/upload', FormDataHandler),
     ], debug=options.debug)
     app.listen(options.port, max_body_size=options.max_body_size)
     IOLoop.instance().start()
