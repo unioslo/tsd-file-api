4473fe65aecf694ccbfe4a2bbae503c5a1d7d7b1
leondutoit <dutoit.leon@gmail.com>
2019-08-08 15:38:38 +0200

    bump version to 1.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tsd-file-api',
-    version='1.2.1',
+    version='1.2.2',
     description='A REST API for handling files and data streams',
     author='Leon du Toit',
     author_email='l.c.d.toit@usit.uio.no',
