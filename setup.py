96f3efc123e04ff5caf2fc36050a8ef8637a71e9
Eirik Haatveit <haatveit@uio.no>
2020-02-26 14:07:32 +0100

    Version 1.7.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tsd-file-api',
-    version='1.7.9',
+    version='1.7.10',
     description='A REST API for handling files and json',
     author='Leon du Toit',
     author_email='l.c.d.toit@usit.uio.no',
