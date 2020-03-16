b36a14ed94159528c81780feb0ff08438df04fba
leondutoit <dutoit.leon@gmail.com>
2020-03-16 22:47:46 +0100

    fix null handling, test deletes, add todos for possible features in filtering, and updating

--- tsdfileapi/test_sql_parser.py
+++ tsdfileapi/test_sql_parser.py
@@ -7,9 +7,10 @@ if __name__ == '__main__':
         {
             'x': 0,
             'y': 1,
-            'z': 5,
+            'z': None,
             'b':[1, 2, 5, 1],
-            'c': None
+            'c': None,
+            'd': 'string1'
         },
         {
             'y': 11,
@@ -31,7 +32,8 @@ if __name__ == '__main__':
                 {
                     'h': 0
                 }
-            ]
+            ],
+            'd': 'string2'
         },
         {
             'a': {
@@ -42,7 +44,8 @@ if __name__ == '__main__':
                 'k2': ['val', 9]
             },
             'z': 0,
-            'x': 88
+            'x': 88,
+            'd': 'string3'
         },
         {
             'a': {
@@ -83,9 +86,16 @@ if __name__ == '__main__':
         '/mytable?select=c[#].h',
         '/mytable?select=c[#].(h,p)',
         '/mytable?select=y,c[#].(h,i)',
-        # filtering - TODO: support OR, and grouped conditions
+        # filtering
+        # TODO: consider supporting OR, and grouped conditions
+        # possibile approach
+        # &where=x=not.is.null
+        # &where=(x=not.like.*zap:and:y=not.is.null)
+        # &where=(x=not.like.*zap:and:y=not.is.null)
+        # &where=((x=not.like.*zap:and:y=not.is.null):or:z=eq.0)
         '/mytable?select=x&z=eq.5&y=gt.0',
         '/mytable?x=not.like.*zap&y=not.is.null',
+        #'/mytable?d=in.(string1,string2)', # TODO, for string only
         '/mytable?select=z&a.k1.r2=eq.2',
         '/mytable?select=z&a.k1.r1[0]=eq.1',
         '/mytable?select=z&a.k3[0].h=eq.0',
@@ -102,13 +112,15 @@ if __name__ == '__main__':
         '/mytable?select=x,c[#].(h,p),a.k1,b[0]&x=not.is.null&order=x.desc&range=1.2'
     ]
     update_uris = [
-        # updates
         '/mytable?set=x.5,y.6&z=eq.5',
-        '/mytable?set=a.k1.r2.5&z=eq.0', # need slicing here
+        # TODO: support
+        # '/mytable?set=a.k1.r2.5&z=eq.0',
+        # '/mytable?set=b[0].677&b=not.is.null',
+        # '/mytable?set=a.k1.r1[0].35',
+        # '/mytable?set=c[0].h.4',
     ]
     delete_uris = [
-        # deletion
-        #'/mytable?z=not.is.null'
+        '/mytable?z=not.is.null'
     ]
     # test regexes for single column selection strategies
     sql = SqlStatement('')
@@ -138,7 +150,7 @@ if __name__ == '__main__':
     print(sql.quote_column('erd.ys[1].(z,a)'))
     print(sql.quote_column('erd.ys[1].z'))
     print(sql.quote_column('"erd"."ys"[1]."z"'))
-    print(sql.smart_split('x,y[1].(f,m,l),z')) # desired: ['x', 'y[1].(f,m,l)', 'z']
+    print(sql.smart_split('x,y[1].(f,m,l),z'))
     print(sql.smart_split('x,y[1].k'))
     #print(sql.quote_column(',x,y[1].(z,a)')) - should error
     for uri in select_uris:
@@ -159,3 +171,9 @@ if __name__ == '__main__':
             print()
         except Exception:
             pass
+    for uri in delete_uris:
+        print(uri)
+        query_engine = sqlite_init('/tmp', name= 'file-api-test.db', builtin=True)
+        print(sqlite_delete_data(query_engine, 'mytable', uri, verbose=True))
+        query_engine = sqlite_init('/tmp', name= 'file-api-test.db', builtin=True)
+        print(sqlite_get_data(query_engine, 'mytable', '/mytable'))
