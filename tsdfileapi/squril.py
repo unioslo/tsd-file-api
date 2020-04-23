175c48c5f9aeec41b856e4f5984ed7910a16e895
leondutoit <dutoit.leon@gmail.com>
2020-04-23 13:56:52 +0200

    move term handlers into generic base class

--- tsdfileapi/squril.py
+++ tsdfileapi/squril.py
@@ -369,11 +369,31 @@ class SqlGenerator(object):
         if not self.json_object_sql:
             msg = 'Extending the SqlGenerator requires setting the class level property: json_object_sql'
             raise Exception(msg)
-        self.select_query = self.gen_sql_select()
-        self.update_query = self.gen_sql_update()
-        self.delete_query = self.gen_sql_delete()
+        self.select_query = self.sql_select()
+        self.update_query = self.sql_update()
+        self.delete_query = self.sql_delete()
 
-    def clause_map_terms(self, clause, map_func):
+    # Classes that extend the SqlGenerator must implement the following methods
+    # they are called by functions that are mapped over terms in clauses
+    # for each term, an appropriate piece of SQL needs to be returned.
+    # What is appropriate, depends on the backend.
+
+    def _gen_sql_key_selection(self, term, parsed):
+        raise NotImplementedError
+
+    def _gen_sql_array_selection(self, term, parsed):
+        raise NotImplementedError
+
+    def _gen_sql_array_sub_selection(self, term, parsed, specific=None):
+        raise NotImplementedError
+
+    def _gen_sql_col(self, term):
+        raise NotImplementedError
+
+    def _gen_sql_update(self, term):
+        raise NotImplementedError
+
+    def _clause_map_terms(self, clause, map_func):
         # apply a function to all Terms in a clause
         out = []
         for term in clause.parsed:
@@ -381,154 +401,33 @@ class SqlGenerator(object):
             out.append(res)
         return out
 
+    # methods for mapping functions over terms in different types of clauses
+
     def select_map(self, map_func):
-        return self.clause_map_terms(self.parsed_uri_query.select, map_func) \
+        return self._clause_map_terms(self.parsed_uri_query.select, map_func) \
             if self.parsed_uri_query.select else None
 
     def where_map(self, map_func):
-        return self.clause_map_terms(self.parsed_uri_query.where, map_func) \
+        return self._clause_map_terms(self.parsed_uri_query.where, map_func) \
             if self.parsed_uri_query.where else None
 
     def order_map(self, map_func):
-        return self.clause_map_terms(self.parsed_uri_query.order, map_func) \
+        return self._clause_map_terms(self.parsed_uri_query.order, map_func) \
             if self.parsed_uri_query.order else None
 
     def range_map(self, map_func):
-        return self.clause_map_terms(self.parsed_uri_query.range, map_func) \
+        return self._clause_map_terms(self.parsed_uri_query.range, map_func) \
             if self.parsed_uri_query.range else None
 
     def set_map(self, map_func):
-        return self.clause_map_terms(self.parsed_uri_query.set, map_func) \
+        return self._clause_map_terms(self.parsed_uri_query.set, map_func) \
             if self.parsed_uri_query.set else None
-
-    # mapper methods - used by public methods
-
-    def _gen_sql_select_clause(self):
-        out = self.select_map(self._term_to_sql_select)
-        if not out:
-            sql_select = f'select * from "{self.table_name}"'
-        else:
-            joined = ",".join(out)
-            sql_select = f"select {self.json_object_sql}({joined}) from \"{self.table_name}\""
-        return sql_select
-
-    def _gen_sql_where_clause(self):
-        out = self.where_map(self._term_to_sql_where)
-        if not out:
-            sql_where = ''
-        else:
-            joined = ' '.join(out)
-            sql_where = f'where {joined}'
-        return sql_where
-
-    def _gen_sql_order_clause(self):
-        out = self.order_map(self._term_to_sql_order)
-        if not out:
-            return ''
-        else:
-            return out[0]
-
-    def _gen_sql_range_clause(self):
-        out = self.range_map(self._term_to_sql_range)
-        if not out:
-            return ''
-        else:
-            return out[0]
-
-    # public methods - called by constructor
-
-    def gen_sql_select(self):
-        _select = self._gen_sql_select_clause()
-        _where = self._gen_sql_where_clause()
-        _order = self._gen_sql_order_clause()
-        _range = self._gen_sql_range_clause()
-        return f'{_select} {_where} {_order} {_range}'
-
-    def gen_sql_update(self):
-        out = self.set_map(self._term_to_sql_update)
-        if not out:
-            return ''
-        else:
-            _set = out[0]
-            _where = self._gen_sql_where_clause()
-            return f'update "{self.table_name}" {_set} {_where}'
-
-    def gen_sql_delete(self):
-        _where = self._gen_sql_where_clause()
-        return f'delete from "{self.table_name}" {_where}'
-
-
-class SqliteQueryGenerator(SqlGenerator):
-
-    """Generate SQL for SQLite json1 backed tables, from a given UriQuery."""
-
-    json_object_sql = 'json_object'
-
-    # Helper functions - used by mappers
-
-    def _gen_sql_key_selection(self, term, parsed):
-        selection = f"\"{parsed.element}\", json_extract(data, '$.{term.original}')"
-        return selection
-
-    def _gen_sql_array_selection(self, term, parsed):
-        selection = f"""
-            \"{parsed.bare_key}\",
-            case when json_extract(data, '$.{term.original}') is not null then
-                json_array(json_extract(data, '$.{term.original}'))
-            else null end
-            """
-        return selection
-
-
-    def _gen_sql_array_sub_selection(self, term, parsed, specific=None):
-        if specific:
-            fullkey = f"and fullkey = '$.{parsed.bare_key}[{parsed.idx}]'"
-        else:
-            fullkey = ''
-        temp = []
-        for key in parsed.sub_selections:
-            temp.append(f"\"{key}\", json_extract(value, '$.{key}')")
-        sub_selections = ','.join(temp)
-        selection = f"""
-                \"{parsed.bare_key}\",
-                (case when json_extract(data, '$.{term.bare_term}') is not null then (
-                    select json_group_array(vals) from (
-                        select json_object(
-                            {sub_selections}) as vals
-                        from (
-                            select key, value, fullkey, path
-                            from \"{self.table_name}\", json_tree(\"{self.table_name}\".data)
-                            where path = '$.{parsed.bare_key}'
-                            {fullkey}
-                            )
-                        )
-                    )
-                else null end)
-            """
-        return selection
-
-    def _gen_sql_col(self, term):
-        if len(term.parsed[0].select_term.parsed) > 1:
-            test_select_term = term.parsed[0].select_term.parsed[-1]
-            if isinstance(test_select_term, ArraySpecific):
-                target = term.parsed[0].select_term.original
-            elif isinstance(test_select_term, ArraySpecificSingle):
-                _key = term.parsed[0].select_term.bare_term
-                _idx = term.parsed[0].select_term.parsed[-1].idx
-                _col = term.parsed[0].select_term.parsed[-1].sub_selections[0]
-                target = f'{_key}[{_idx}].{_col}'
-            else:
-                raise Exception(f'Unsupported term {term.original}')
-        else:
-            if not isinstance(term.parsed[0].select_term.parsed[0], Key):
-                raise Exception(f'Invalid term {term.original}')
-            target = term.parsed[0].select_term.parsed[0].element
-        col = f"json_extract(data, '$.{target}')"
-        return col
 
     # term handler functions
     # mapped over terms in a clause
     # generates SQL for each term
+    # SQL is generated by calling other functions
+    # which are implemented for specific SQL backend implementations
 
     def _term_to_sql_select(self, term):
         rev = term.parsed.copy()
@@ -604,6 +503,134 @@ class SqliteQueryGenerator(SqlGenerator):
     def _term_to_sql_update(self, term):
         if not self.data:
             return None
+        out = self._gen_sql_update(term)
+        return out
+
+    # mapper methods - used by public methods
+
+    def _gen_sql_select_clause(self):
+        out = self.select_map(self._term_to_sql_select)
+        if not out:
+            sql_select = f'select * from "{self.table_name}"'
+        else:
+            joined = ",".join(out)
+            sql_select = f"select {self.json_object_sql}({joined}) from \"{self.table_name}\""
+        return sql_select
+
+    def _gen_sql_where_clause(self):
+        out = self.where_map(self._term_to_sql_where)
+        if not out:
+            sql_where = ''
+        else:
+            joined = ' '.join(out)
+            sql_where = f'where {joined}'
+        return sql_where
+
+    def _gen_sql_order_clause(self):
+        out = self.order_map(self._term_to_sql_order)
+        if not out:
+            return ''
+        else:
+            return out[0]
+
+    def _gen_sql_range_clause(self):
+        out = self.range_map(self._term_to_sql_range)
+        if not out:
+            return ''
+        else:
+            return out[0]
+
+    # public methods - called by constructor
+
+    def sql_select(self):
+        _select = self._gen_sql_select_clause()
+        _where = self._gen_sql_where_clause()
+        _order = self._gen_sql_order_clause()
+        _range = self._gen_sql_range_clause()
+        return f'{_select} {_where} {_order} {_range}'
+
+    def sql_update(self):
+        out = self.set_map(self._term_to_sql_update)
+        if not out:
+            return ''
+        else:
+            _set = out[0]
+            _where = self._gen_sql_where_clause()
+            return f'update "{self.table_name}" {_set} {_where}'
+
+    def sql_delete(self):
+        _where = self._gen_sql_where_clause()
+        return f'delete from "{self.table_name}" {_where}'
+
+
+class SqliteQueryGenerator(SqlGenerator):
+
+    """Generate SQL for SQLite json1 backed tables, from a given UriQuery."""
+
+    json_object_sql = 'json_object'
+
+    # Helper functions - used by mappers
+
+    def _gen_sql_key_selection(self, term, parsed):
+        selection = f"\"{parsed.element}\", json_extract(data, '$.{term.original}')"
+        return selection
+
+    def _gen_sql_array_selection(self, term, parsed):
+        selection = f"""
+            \"{parsed.bare_key}\",
+            case when json_extract(data, '$.{term.original}') is not null then
+                json_array(json_extract(data, '$.{term.original}'))
+            else null end
+            """
+        return selection
+
+    def _gen_sql_array_sub_selection(self, term, parsed, specific=None):
+        if specific:
+            fullkey = f"and fullkey = '$.{parsed.bare_key}[{parsed.idx}]'"
+        else:
+            fullkey = ''
+        temp = []
+        for key in parsed.sub_selections:
+            temp.append(f"\"{key}\", json_extract(value, '$.{key}')")
+        sub_selections = ','.join(temp)
+        selection = f"""
+                \"{parsed.bare_key}\",
+                (case when json_extract(data, '$.{term.bare_term}') is not null then (
+                    select json_group_array(vals) from (
+                        select json_object(
+                            {sub_selections}) as vals
+                        from (
+                            select key, value, fullkey, path
+                            from \"{self.table_name}\", json_tree(\"{self.table_name}\".data)
+                            where path = '$.{parsed.bare_key}'
+                            {fullkey}
+                            )
+                        )
+                    )
+                else null end)
+            """
+        return selection
+
+    def _gen_sql_col(self, term):
+        if len(term.parsed[0].select_term.parsed) > 1:
+            test_select_term = term.parsed[0].select_term.parsed[-1]
+            if isinstance(test_select_term, ArraySpecific):
+                target = term.parsed[0].select_term.original
+            elif isinstance(test_select_term, ArraySpecificSingle):
+                _key = term.parsed[0].select_term.bare_term
+                _idx = term.parsed[0].select_term.parsed[-1].idx
+                _col = term.parsed[0].select_term.parsed[-1].sub_selections[0]
+                target = f'{_key}[{_idx}].{_col}'
+            else:
+                raise Exception(f'Unsupported term {term.original}')
+        else:
+            if not isinstance(term.parsed[0].select_term.parsed[0], Key):
+                raise Exception(f'Invalid term {term.original}')
+            target = term.parsed[0].select_term.parsed[0].element
+        col = f"json_extract(data, '$.{target}')"
+        return col
+
+    def _gen_sql_update(self, term):
         key = term.parsed[0].select_term.bare_term
         assert self.data.get(key) is not None, f'Target key of update: {key} not found in payload'
         assert len(self.data.keys()) == 1, f'Cannot update more than one key per statement'
