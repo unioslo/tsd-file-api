98f926a2e36dbab15ea8e04ae2a2cc12d8f26020
leondutoit <dutoit.leon@gmail.com>
2020-06-30 14:22:48 +0200

    fix default config

--- tsdfileapi/defaults.py
+++ tsdfileapi/defaults.py
@@ -19,6 +19,11 @@ _config = {
     'create_tenant_dir': True,
     'jwt_test_secret': 'jS25aQbePizfTsetg8LbFsNKl1W6wi4nQaBj705ofWA=',
     'jwt_secret': None,
+    'nacl_public': {
+        'public': 'mZQEzkyi7bCvmDVfHGsU/7HX1+gT/R3PkSnyDU4OaiY=',
+        'private': 'fTEB1MZz8MskkZHSIM9ypxJc4e45Z8fmLGGXkUrp1hQ='
+    },
+    'log_level': 'info',
     'backends': {
         'disk': {
             'store': {
@@ -68,14 +73,14 @@ _config = {
                 },
             },
         },
-        'sqlite': {
-            'generic': {
-                'db_path': '/tmp/pXX',
-                'table_structure': None,
-            },
+        'dbs': {
             'apps_tables': {
-                'db_path': '/tmp/pXX',
-                'table_structure': None,
+                'db': {
+                    'engine': 'sqlite',
+                    'path': '/tmp/pXX',
+                    'table_structure': None,
+                    'mq': None
+                }
             },
         }
     }
