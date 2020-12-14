5eb07e343eba9a27083cfd3ea5a471c8e2aee607
leondutoit <dutoit.leon@gmail.com>
2020-12-14 11:20:09 +0100

    allow apps to get the libsodium key

--- tsdfileapi/api.py
+++ tsdfileapi/api.py
@@ -2543,6 +2543,7 @@ class Backends(object):
             ('/v1/(.*)/apps/(.+)/tables/audit', GenericTableHandler, dict(backend='apps_tables')),
             ('/v1/(.*)/apps/(.+)/tables/metadata', GenericTableHandler, dict(backend='apps_tables')),
             ('/v1/(.*)/apps/.+/tables/(.+)$', GenericTableHandler, dict(backend='apps_tables')),
+            ('/v1/(.*)/apps/crypto/key', NaclKeyHander),
         ]
     }
 
