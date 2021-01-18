1e6ce0f3f5d262a763b406c48af9f7ce646a74d4
leondutoit <dutoit.leon@gmail.com>
2021-01-18 10:21:42 +0100

    remove cluster urls and tests

--- tsdfileapi/utils.py
+++ tsdfileapi/utils.py
@@ -49,18 +49,6 @@ def check_filename(filename, disallowed_start_chars=[]):
     return filename
 
 
-def create_cluster_dir_if_not_exists(path, tenant, tenant_string_pattern):
-    # TODO: need to move the /file-import to config
-    base = path.replace(tenant_string_pattern, tenant).replace('/file-import', '')
-    target = path.replace(tenant_string_pattern, tenant)
-    if os.path.lexists(base):
-        if not os.path.lexists(target):
-            os.makedirs(target)
-        return target
-    else:
-        raise Exception('{0} does not have a cluster disk space'.format(tenant))
-
-
 def sns_dir(base_pattern, tenant, uri, tenant_string_pattern, test=False):
     """
     Construct a path for sns uploads.
