c5840dc0f49fab6c81881e42d81789e1da7273dd
leondutoit <dutoit.leon@gmail.com>
2022-10-16 16:53:18 +0200

    define more exceptions

--- tsdfileapi/exc.py
+++ tsdfileapi/exc.py
@@ -1,15 +1,30 @@
 
-"""Error definitions for failure modes for which we can determine the cause."""
+"""
+Error definitions for failure modes for which we can determine the cause,
+mapped to HTTP status codes.
 
+OSError notes:
+
+[Errno 2]   | errno.ENOENT | No such file or directory (NFS mount issue)
+[Errno 122] | errno.EDQUOT | Disk quota exceeded
+
+
+References:
+- https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+- https://httpwg.org/specs/rfc9110.html#overview.of.status.codes
+
+"""
+
+from http import HTTPStatus
 
 class ApiError(Exception):
 
     desc = "API error"
-    sc = None
+    status = None
 
-    def __init__(self, message: str = "", status: int = 500) -> None:
-        self.message = message or self.desc
-        self.sc = self.sc or status
+    def __init__(self, message: str = "API Error") -> None:
+        self.message = message
+        self.log_msg = f"{self.desc}, {self.status.phrase}, {self.message}"
 
 
 # Client errors -  HTTP 4XX range
@@ -17,15 +32,19 @@ class ApiError(Exception):
 
 class ClientError(ApiError):
     desc = "Client error"
-    sc = 400
+    status = HTTPStatus.BAD_REQUEST
 
 class ClientIllegalFilenameError(ClientError):
     desc = "Filename not allowed"
-    sc = 400
+    status = HTTPStatus.BAD_REQUEST
 
 class ClientSnsPathError(ClientError):
     desc = "Wrong URL path for sns backend"
-    sc = 400
+    status = HTTPStatus.BAD_REQUEST
+
+class ClientAuthorizationError(ClientError):
+    desc = "Client not authorized for request"
+    status = HTTPStatus.FORBIDDEN
 
 
 # Server errors - HTTP 5XX range
@@ -33,16 +52,24 @@ class ClientSnsPathError(ClientError):
 
 class ServerError(ApiError):
     desc = "Server error"
-    sc = 500
+    status = HTTPStatus.INTERNAL_SERVER_ERROR
 
 class ServerStorageTemporarilyUnavailableError(ServerError):
-    desc = "Raised for backends which cannot be used during migration"
-    sc = 503
+    desc = "Backend cannot be used during migration"
+    status = HTTPStatus.SERVICE_UNAVAILABLE
 
 class ServerStorageNotMountedError(ServerError):
-    desc = "NFS mount missing"
-    sc = 500
+    desc = "errno.ENOENT, NFS mount missing"
+    status = HTTPStatus.INTERNAL_SERVER_ERROR
 
 class ServerSnsError(ServerError):
     desc = "Issue contructing sns storage path"
-    sc = 500
+    status = HTTPStatus.INTERNAL_SERVER_ERROR
+
+class ServerDiskQuotaExceededError(ServerError):
+    desc = "errno.EDQUOT, Project has run out of disk quota"
+    status = HTTPStatus.INSUFFICIENT_STORAGE
+
+class ServerMaintenanceError(ServerError):
+    desc = "Server down for maintenance"
+    status = HTTPStatus.SERVICE_UNAVAILABLE
