2936fae0da6da1de7b7816d7e958860a08548517
leondutoit <dutoit.leon@gmail.com>
2024-06-21 10:33:48 +0200

    a better migration script

--- scripts/migrate-tables.sql
+++ scripts/migrate-tables.sql
@@ -12,30 +12,38 @@ create or replace function migrate_tables(target text)
         if target = 'survey' then
             for _schema, _table_name in
                 select table_schema, table_name from information_schema.tables
-                    where table_schema ~ 'p|ec[0-9]+'
-                    and table_name ~ '.+_(audit|metadata)'
+                    where table_schema ~ '(p|ec)[0-9]+'
+                    and table_name ~ '.+_(audit|metadata|tasks).*'
             loop
-                raise info 'migrating: %', _table_name;
-                _new_name := replace(_table_name, '_', '/');
-                execute format(
-                    'alter table %s."%s" rename to "%s"',
-                    _schema, _table_name, _new_name
-                );
+                begin
+                    _new_name := replace(_table_name, '_', '/');
+                    execute format(
+                        'alter table %s."%s" rename to "%s"',
+                        _schema, _table_name, _new_name
+                    );
+                    raise info 'migrated: %', _table_name;
+                exception when others then
+                    raise info 'error migrating: %', _table_name;
+                end;
             end loop;
         elsif target = 'apps' then
             for _schema, _table_name in
                 select table_schema, table_name from information_schema.tables
-                    where table_schema ~ 'p|ec[0-9]+_.+'
+                    where table_schema ~ '(p|ec)[0-9]+_.+'
                     and table_name ~ '.+_audit' -- for all
                     or table_name ~ 'persons_.+' -- mvh
-                    or table_name ~ '.+_memos' -- tables | ros
+                    or table_name ~ '.+_(memos|meta)' -- tables | ros
             loop
-                raise info 'migrating: %', _table_name;
-                _new_name := replace(_table_name, '_', '/');
-                execute format(
-                    'alter table %s."%s" rename to "%s"',
-                    _schema, _table_name, _new_name
-                );
+                begin
+                    _new_name := replace(_table_name, '_', '/');
+                    execute format(
+                        'alter table %s."%s" rename to "%s"',
+                        _schema, _table_name, _new_name
+                    );
+                    raise info 'migrated: %', _table_name;
+                exception when others then
+                    raise info 'error migrating: %', _table_name;
+                end;
             end loop;
         end if;
     end;
