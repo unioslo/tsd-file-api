7f8cb75c501bde91b796d786b7ce9dbab78bf8d8
leondutoit <dutoit.leon@gmail.com>
2025-11-20 13:34:22 +0100

    remove sqlalchemy from db.py

--- tsdfileapi/db.py
+++ tsdfileapi/db.py
@@ -1,18 +1,8 @@
 import logging
 import sqlite3
-from contextlib import contextmanager
-from typing import ContextManager
-from typing import Union
 
 import psycopg2.extensions
 import psycopg2.pool
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy.exc import IntegrityError
-from sqlalchemy.exc import OperationalError
-from sqlalchemy.exc import StatementError
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import QueuePool
 
 logger = logging.getLogger(__name__)
 
@@ -33,13 +23,8 @@ def sqlite_init(
     path: str,
     name: str = "api-data.db",
     builtin: bool = False,
-) -> Union[sqlalchemy.engine.Engine, sqlite3.Connection]:
-    dbname = name
-    if not builtin:
-        dburl = "sqlite:///" + path + "/" + dbname
-        engine = create_engine(dburl, poolclass=QueuePool)
-    else:
-        engine = sqlite3.connect(path + "/" + dbname)
+) -> sqlite3.Connection:
+    engine = sqlite3.connect(f"{path}/{name}")
     return engine
 
 
@@ -49,19 +34,3 @@ def postgres_init(
     dsn = f"dbname={dbconfig['dbname']} user={dbconfig['user']} password={dbconfig['pw']} host={dbconfig['host']}"
     pool = psycopg2.pool.SimpleConnectionPool(min_conn, max_conn, dsn)
     return pool
-
-
-@contextmanager
-def session_scope(
-    engine: sqlalchemy.engine.Engine,
-) -> ContextManager[sqlalchemy.orm.session.Session]:
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    try:
-        yield session
-        session.commit()
-    except (OperationalError, IntegrityError, StatementError) as e:
-        session.rollback()
-        raise e
-    finally:
-        session.close()
