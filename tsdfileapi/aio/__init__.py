5bcb95d5358f04625c0246fb2e3557ffbc6f13ee
Armen Michaeli <armen.michaeli@usit.uio.no>
2026-06-05 11:17:35 +0200

    fixup! Add the `aio` module for encapsulation of asynchronous utilities
    Don't like repeating myself, besides I did the "noqa: F401" thing in
    another spot just like it. To explain -- the directive makes Ruff ignore
    the fact we're intentionally exporting an element imported from another
    module -- Ruff doesn't like the code by default because it is of the
    opinion we're making an omission, but in this case we're not.

--- tsdfileapi/aio/__init__.py
+++ tsdfileapi/aio/__init__.py
@@ -1,5 +1,5 @@
 import aioshutil as shutil  # noqa: F401
 from aiofiles import *  # noqa: F403
-from asyncstdlib import enumerate as enumerate
+from asyncstdlib import enumerate  # noqa: F401
 
 from . import os as os
